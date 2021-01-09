3ee3b777e2b8cfb293191d9be54b5a0ded4c32e4
Lucimar Neves <lucimar.neves@gmail.com>
2021-01-08 22:47:59 -0300

    Add Navbar

--- sds2-aulas/front-web/src/App.tsx
+++ sds2-aulas/front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './Navbar';
 import './App.css';
 
 
@@ -6,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
     </div>
   );
 }
