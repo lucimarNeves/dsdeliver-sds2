2760564cade3db4bca782d9971af25dd12e3e35a
Lucimar Neves <lucimar.neves@gmail.com>
2021-01-09 18:18:30 -0300

    Search order location

--- sds2-aulas/front-web/src/Orders/index.tsx
+++ sds2-aulas/front-web/src/Orders/index.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
+import OrderLocation from './OrderLocation';
 import './styles.css';
+import { Product } from './types';
+import { OrderLocationdata } from './types';
+import { fetchProducts } from '../api';
+
+
 
 
 function Orders(){
+
+  const[products, setProducts] = useState<Product[]>([]);
+  const[orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+  
+
+   useEffect(() => {
+    
+    fetchProducts()
+    .then(response => setProducts(response.data))
+    .catch(error => console.log(error));
+    
+  }, []);
+
     return (
       <div className="orders-container">
          <StepsHeader />
+         <ProductsList products ={products} />
+         <OrderLocation onChangeLocation ={ location => setOrderLocation(location)} />
+
       </div> 
     )
 }
